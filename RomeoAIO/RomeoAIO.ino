664251515a32bc83970f98b0295d2f72ee462a49
Ricardo Martinelli de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2015-01-10 23:11:52 -0200

    Wrong sketch, updating with the correct one

--- RomeoAIO/RomeoAIO.ino
+++ RomeoAIO/RomeoAIO.ino
@@ -1,59 +1,87 @@
-//Standard DLL Speed control
-
-int E1 = 4;     //M1 Speed Control
-int E2 = 7;     //M2 Speed Control
-int M1 = 5;    //M1 Direction Control
-int M2 = 6;    //M1 Direction Control
-
+/** Robot Arduino
+  * 
+  */
+int E1 = 5;     //M1 Speed Control
+int E2 = 6;     //M2 Speed Control
+int M1 = 4;    //M1 Direction Control
+int M2 = 7;    //M1 Direction Control
+ 
 ///For previous Romeo, please use these pins.
 //int E1 = 6;     //M1 Speed Control
 //int E2 = 9;     //M2 Speed Control
 //int M1 = 7;    //M1 Direction Control
 //int M2 = 8;    //M1 Direction Control
-
-//When m1p/m2p is 127, it stops the motor
-//when m1p/m2p is 255, it gives the maximum speed for one direction
-//When m1p/m2p is 0, it gives the maximum speed for reverse direction
-
-void DriveMotorP(byte m1p, byte m2p)//Drive Motor Power Mode
+ 
+ 
+void stop(void)                    //Stop
 {
-
-  digitalWrite(E1, HIGH);
-  analogWrite(M1, (m1p)); 
-
-  digitalWrite(E2, HIGH);
-  analogWrite(M2, (m2p)); 
-
+  digitalWrite(E1,LOW);   
+  digitalWrite(E2,LOW);      
+}   
+void advance(char a,char b)          //Move forward
+{
+  analogWrite (E1,a);      //PWM Speed Control
+  digitalWrite(M1,HIGH);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,HIGH);
+}  
+void back_off (char a,char b)          //Move backward
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);   
+  analogWrite (E2,b);    
+  digitalWrite(M2,LOW);
 }
-
+void turn_L (char a,char b)             //Turn Left
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,HIGH);
+}
+void turn_R (char a,char b)             //Turn Right
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,HIGH);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,LOW);
+}
 void setup(void) 
 { 
   int i;
-  for(i=6;i<=9;i++)
+  for(i=4;i<=7;i++)
     pinMode(i, OUTPUT);  
   Serial.begin(19200);      //Set Baud Rate
+  Serial.println("Run keyboard control");
 } 
 void loop(void) 
-{ 
+{
   if(Serial.available()){
     char val = Serial.read();
-    if(val!=-1)
+    if(val != -1)
     {
       switch(val)
       {
       case 'w'://Move Forward
-        DriveMotorP(0xff,0xff); // Max speed
+        advance (255,255);   //move forward in max speed
         break;
-      case 'x'://Move Backward
-        DriveMotorP(0x00,0x00);
-        ; // Max speed
+      case 's'://Move Backward
+        back_off (255,255);   //move back in max speed
         break;
-      case 's'://Stop
-        DriveMotorP(0x7f,0x7f);
+      case 'a'://Turn Left
+        turn_L (100,100);
         break;       
-
+      case 'd'://Turn Right
+        turn_R (100,100);
+        break;
+      case 'z':
+        Serial.println("Hello");
+        break;
+      case 'x':
+        stop();
+        break;
       }
     }
+    else stop();  
   }
 }
-
