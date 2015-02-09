316910570785d8bf3ff0668f6376856f49501066
Ricardo M. de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2015-02-09 00:09:49 -0200

    Adding LCD and Sensors code

--- RomeoAIO/RomeoAIO.ino
+++ RomeoAIO/RomeoAIO.ino
@@ -1,12 +1,13 @@
 /** Robot Arduino
-  * 
-  */
+ * 
+ */
+#include <LiquidCrystal.h>
 
 // Motores
-#define E1 5    //M1 Speed Control
-#define E2 6    //M2 Speed Control
-#define M1 4    //M1 Direction Control
-#define M2 7    //M2 Direction Control
+#define M1 4 //M1 Direction Control
+#define E1 5 //M1 Speed Control
+#define E2 6 //M2 Speed Control
+#define M2 7 //M2 Direction Control
  
 // Sensores
 #define S1  8
@@ -14,75 +15,65 @@
 #define S3 10
 #define S4 11
 
+// Direcao
+#define FORWARD  0
+#define BACKWARD 1
+#define LEFT     2
+#define RIGHT    3
+
+// Display
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+int state = 0;
+
 void stop(void) {
   digitalWrite(E1, LOW);   
   digitalWrite(E2, LOW);      
 }
 
-void forward(char a, char b) {
-  analogWrite (E1, a);      //PWM Speed Control
-  digitalWrite(M1, HIGH);    
-  analogWrite (E2, b);    
-  digitalWrite(M2, HIGH);
-}  
-
-void backward(char a, char b) {
-  analogWrite (E1, a);
-  digitalWrite(M1, LOW);   
-  analogWrite (E2, b);    
-  digitalWrite(M2, LOW);
+void drive(char left, char right, int direction) {
+  analogWrite (E1, left);
+  analogWrite (E2, right);    
+  
+  switch(direction) {
+  case FORWARD:
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    break;
+  case BACKWARD:
+    digitalWrite(M1, LOW);
+    digitalWrite(M2, LOW);
+    break;
+  case LEFT:
+    digitalWrite(M1, LOW);
+    digitalWrite(M2, HIGH);
+    break;
+  case RIGHT:
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, LOW);
+    break;
+  }
 }
 
-void turnLeft(char a,char b) {
-  analogWrite (E1,a);
-  digitalWrite(M1,LOW);    
-  analogWrite (E2,b);    
-  digitalWrite(M2,HIGH);
-}
-
-void turnRight(char a,char b) {
-  analogWrite (E1,a);
-  digitalWrite(M1,HIGH);    
-  analogWrite (E2,b);    
-  digitalWrite(M2,LOW);
+void printMenu() {
+  lcd.setCursor(0,0);
+  lcd.print("Robot-arduino");
 }
 
 void setup(void) { 
   int i;
-  for(i=M1;i<=M2;i++) // Setando saida motores
+  for(i=M1; i<=M2; i++)
     pinMode(i, OUTPUT);
-  for(i=S1;i<=S4;i++) // Setando entrada sensores
+  for(i=S1; i<=S4; i++)
     pinMode(i, INPUT);  
-  Serial.begin(19200);      //Set Baud Rate
-  Serial.println("Run keyboard control");
+  lcd.begin(0,0); 
 }
 
 void loop(void) {
-  if(Serial.available()){
-    char val = Serial.read();
-    if(val != -1) {
-      switch(val) {
-      case 'w'://Move Forward
-        forward(255,255);   //move forward in max speed
-        break;
-      case 's'://Move Backward
-        backward(255,255);   //move back in max speed
-        break;
-      case 'a'://Turn Left
-        turnLeft(100,100);
-        break;       
-      case 'd'://Turn Right
-        turnRight(100,100);
-        break;
-      case 'z':
-        Serial.println("Hello");
-        break;
-      case 'x':
-        stop();
-        break;
-      }
-    }
-    else stop();  
+  switch(state) {
+  case 0:
+      printMenu();
+      break;
   }
   
 }
